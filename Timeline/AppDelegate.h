d4febba11eee194a02240635efe893196b96e879
Jim <yiwei.wu@szxiaojun.com>
2019-03-30 19:30:25 +0800

    Initial Commit

---
+++ Timeline/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Timeline
+//
+//  Created by Jim on 2019/3/30.
+//  Copyright © 2019 Jim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Timeline/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Timeline
+//
+//  Created by Jim on 2019/3/30.
+//  Copyright © 2019 Jim. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
